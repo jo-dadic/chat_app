a9ca9d2b13c3ba6b81920b3ddf1e13bef0c505d0
Josipa Dadić <josipa.dadic@gmail.com>
2021-05-16 14:40:43 +0200

    images resized

--- src/components/Chat/Message/Message.js
+++ src/components/Chat/Message/Message.js
@@ -23,7 +23,7 @@ const Message = (props) => {
       <img
         src={url}
         alt="new-img"
-        style={{ width: "200px", height: "250px" }}
+        style={{ maxWidth: "500px", maxHeight: "70vh" }}
         onClick={modalHandler}
       />
     );
